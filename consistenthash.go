c28f95501e589ee79b9f91f98675a13a711fec16
Simon Gauld <simongauld@covey.home>
2021-01-19 00:09:55 +0000

    moved some aoc utilities over

---
+++ consistenthash.go
@@ -0,0 +1,41 @@
+package goutils
+
+/*
+type Event struct {
+	name        string
+	probability float64 // the chance of this event occurring
+}
+
+type ConsistentHash struct {
+	events []Event
+	increment float
+}
+
+func (c ConsistentHash) New([]events Event) int {
+	smallestPrecision = 0
+	for index:=0;index<len(events; index++) {
+		decimalPlaces := CountDecimalPlaces(events[index].probabiliity)
+		if decimalPlaces > smallestPrecision {
+			smallestPrecision = decimalPlaces
+		}
+	}
+	smallestIncrement >> smallestPrecision
+	return ConsistentHash{events, smallestIncrement}
+}
+
+// Get returns an Event
+func (c ConsistentHash) Get(value float64) int {
+	for index=0; index<100; index+=c.smallestIncrement
+
+	return 0
+}
+
+func CountDecimalPlaces(v float64) int {
+    s := strconv.FormatFloat(v, 'f', -1, 64)
+    i := strings.IndexByte(s, '.')
+    if i > -1 {
+        return len(s) - i - 1
+    }
+    return 0
+}
+*/
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/simonski/goutils
 
 go 1.14
+
+require (
+	github.com/shirou/gopsutil v3.20.12+incompatible
+	golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78 // indirect
+)
