d4111fb975d9d2b28592a2945158dcb2fae4069a
Simon Gauld <simongauld@covey.home>
2021-10-06 13:58:04 +0100

    package management misunderstanding

--- table_test.go
+++ table_test.go
@@ -5,16 +5,16 @@ import (
 	"testing"
 )
 
-func TestTable(t *testing.T) {
+func TestTable(test *testing.T) {
 	fmt.Printf("Hello, World!\n")
 
-	t := table.NewTable()
-	t.Add(&table.Column{Position: 0, Title: "Fred", WidthPercent: 10})
-	t.Add(&table.Column{Position: 0, Title: "Jack", WidthPercent: 20})
-	t.Add(&table.Column{Position: 0, Title: "Pete", WidthPercent: 30})
-	t.Add(&table.Column{Position: 0, Title: "Jim", WidthPercent: 20})
-	t.Add(&table.Column{Position: 0, Title: "Rob", WidthPercent: 10})
-	t.Add(&table.Column{Position: 0, Title: "Finbar", WidthPercent: 10})
+	t := NewTable()
+	t.Add(&Column{Position: 0, Title: "Fred", WidthPercent: 10})
+	t.Add(&Column{Position: 0, Title: "Jack", WidthPercent: 20})
+	t.Add(&Column{Position: 0, Title: "Pete", WidthPercent: 30})
+	t.Add(&Column{Position: 0, Title: "Jim", WidthPercent: 20})
+	t.Add(&Column{Position: 0, Title: "Rob", WidthPercent: 10})
+	t.Add(&Column{Position: 0, Title: "Finbar", WidthPercent: 10})
 	fmt.Printf("%v\n", t.Header())
 
 }
